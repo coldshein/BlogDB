211013b8d017a4e11203b55fbc4ceaa36a15da32
Roman <coldshein@gmail.com>
2023-06-10 23:31:42 +0300

    CRUD, Multer init

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,6 @@
 import PostModel from "../models/Post.js"
 
-export const getAll = async (req,res) => {
+export const getAll = async (req, res) => {
     try {
         const posts = await PostModel.find().populate('user').exec();
 
@@ -12,6 +12,71 @@ export const getAll = async (req,res) => {
         })
     }
 }
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndUpdate(
+            {
+              _id: postId,
+            },
+            {
+              $inc: { viewsCount: 1 },
+            },
+            {
+              returnDocument: 'after',
+            }
+          )
+            .then(doc => {
+              if (!doc) {
+                return res.status(404).json({
+                  message: 'Can not find post',
+                });
+              }
+              res.json(doc);
+            })
+            .catch(err => {
+              console.log(err);
+              res.status(500).json({
+                message: 'Failed to get post',
+              });
+            });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to get post',
+        });
+    }
+}
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndDelete({ _id: postId })
+            .then((doc) => {
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'Can not find post',
+                    });
+                }
+                res.json({
+                    success: true,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json({
+                    message: 'Failed to remove post',
+                });
+            });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to remove post',
+        });
+    }
+};
+
 
 
 export const create = async (req, res) => {
@@ -30,6 +95,33 @@ export const create = async (req, res) => {
         console.log(error);
         res.status(500).json({
             message: 'Failed post',
+        })
+    }
+}
+
+export const update = async (req,res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModel.updateOne({
+            _id: postId,
+        },
+        {
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,
+        })
+
+        res.json({
+            success: true,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Can not edit post"
         })
     }
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
+import multer from 'multer';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validation.js';
 
@@ -11,6 +12,7 @@ import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controllers/UserController.js'
 import * as postController from './controllers/PostController.js'
+import handleValidationErrors from './utils/handleValidationErrors.js';
 
 
 
@@ -20,18 +22,39 @@ mongoose.connect('mongodb+srv://admin:admin123@coldcluster.7vq6x0r.mongodb.net/b
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+    destination: (i, s, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (i, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
 
-app.post('/auth/login',loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register); 
+const upload = multer({storage});
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login',loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors,  UserController.register); 
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post('/posts', checkAuth, postCreateValidation, postController.create)
+app.post('/upload',checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+} )
+
 app.get('/posts', postController.getAll )
+app.get('/posts/:id', postController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, postController.create)
+app.delete('/posts/:id',checkAuth, postController.remove)
+app.patch('/posts/:id',checkAuth, postCreateValidation, handleValidationErrors, postController.update)
 
 app.listen(4444, (err) => {
     if (err) {
         return console.log(err)
     }
     console.log('Server has been started working...')
-})
+});
